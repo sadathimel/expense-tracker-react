e0629e0baa9444adee0b3df51af9446807a80dd7
sadat <sadat.himel@gmail.com>
2020-10-08 13:20:48 +0600

    add text , amount and amount color dynamic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
-import AddTransaction from './components/AddTransaction';
-import Balance from './components/Balance/Balance';
-import Header from './components/Header/Header';
-import IncomeExpenses from './components/IncomeExpenses';
-import TransactionList from './components/TransactionList';
+import React from "react";
+import "./App.css";
+import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance/Balance";
+import { GlobalProvider } from "./components/context/GlobalState";
+import Header from "./components/Header/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
-    <div >
+    <GlobalProvider>
       <Header></Header>
       <div className="container">
         <Balance></Balance>
@@ -16,7 +17,7 @@ function App() {
         <TransactionList></TransactionList>
         <AddTransaction></AddTransaction>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
